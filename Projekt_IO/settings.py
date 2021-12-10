9d633626003b104f0ef35daadedf204ec3dbf203
Tomasz Rzepka <tomasz.rzepka52@gmail.com>
2021-12-10 16:26:59 +0100

    DSfdsf

--- Projekt_IO/settings.py
+++ Projekt_IO/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
 
     #own apps
     'snake',
