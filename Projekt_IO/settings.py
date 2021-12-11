76f9f9437866888c9b809a2340b21e8c3e7a57f8
Tomasz Rzepka <tomasz.rzepka52@gmail.com>
2021-12-11 19:53:56 +0100

    sdf

--- Projekt_IO/settings.py
+++ Projekt_IO/settings.py
@@ -125,5 +125,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
