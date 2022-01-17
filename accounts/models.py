8f68ff62f449d62d92774de11869b72126d79e62
Tomasz Rzepka <tomasz.rzepka52@gmail.com>
2022-01-17 20:08:17 +0100

    To be dismissed

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,12 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-class CustomUser(AbstractUser):
+class CustomUser(models.Model):
     age = models.PositiveIntegerField(null=True, blank=True)
-    dark_theme = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("signup", args=[str(self.id)])
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import ChangeTheme, SignUpView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),    
+    path('settings/<int:pk>/edit/', ChangeTheme.as_view(), name='settings'),    
 ]
