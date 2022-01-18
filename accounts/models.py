7d0634c187f60df725a99376d41a69101aa61a6b
Tomasz Rzepka <tomasz.rzepka52@gmail.com>
2022-01-18 09:55:55 +0100

    Added settings option

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
     dark_theme = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
+    def get_absolute_url(self): 
+        return reverse("settings", args=[str(self.id)])
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import ChangeTheme, SignUpView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),    
+    path("<int:pk>/", ChangeTheme.as_view(), name='settings'),    
 ]
