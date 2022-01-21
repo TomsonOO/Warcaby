d0b3c3a0d669d5a5ab2f7d463cdfd32750efa71d
Tomasz Rzepka <tomasz.rzepka52@gmail.com>
2022-01-21 15:34:46 +0100

    Added comments in tests

--- menu/tests.py
+++ menu/tests.py
@@ -2,54 +2,47 @@ from django.test import TestCase, SimpleTestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-
-# Create your tests here.
-
+# Test strony głównej
 class HomePageTests(SimpleTestCase):
 
+    # Sprawdza czy stronka zwraca kod 200 - czy istnieje
     def test_home_page_status_code(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
+    # Sprawdza czy zgadza się adres url
     def test_view_url_by_name(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
-
+    # Sprawdza czy załadowano odpowiednią templatke
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
 
-
-class SettingsTests(SimpleTestCase):
-
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse("settings"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse("settings"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "settings.html")
-
+# Test tworzenia konta 
 class SignupPageTests(TestCase):
 
     username = "newuser"
     email    = "newuser@email.com"
 
+    # Sprawdza czy stronka zwraca kod 200 - czy istnieje
     def test_signup_page_status_code(self):
         response = self.client.get("/accounts/signup/")
         self.assertEqual(response.status_code, 200)
 
+    # Sprawdza czy zgadza się adres url
     def test_view_url_by_name(self):
         response = self.client.get(reverse("signup"))
         self.assertEqual(response.status_code, 200)
 
+    # Sprawdza czy załadowano odpowiednią templatke
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse("signup"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "registration/signup.html")
     
+    # Przeprowadza testową rejestrację - tworzy uzytkownika, i po przeprowadzeniu testu go usuwa
     def test_signup_form(self):
         new_user = get_user_model().objects.create_user( self.username, self.email)
         self.assertEqual(get_user_model().objects.all().count(), 1)
